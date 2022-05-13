296e3606efdfda967bb4dc42fb3028297d19dcfb
b3rkin <zeybekogluberkin@metu.edu.tr>
2022-05-13 12:41:27 +0200

    Made gui for training and main page.

--- app/src/main/java/com/example/sps_app_2/MainActivity.java
+++ app/src/main/java/com/example/sps_app_2/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.sps_app_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declare the widget variables.
+    private Button buttonTest;
+    private Button buttonTrain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Declare the widget variables.
+        // findViewById allows us to grab the button from vies.
+        buttonTest = (Button) findViewById(R.id.buttonTraining);
+        buttonTrain = (Button) findViewById(R.id.buttonTesting);
+
+
+        buttonTrain.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                switchToTrainingActivity();
+            }
+        });
+
+        buttonTest.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                switchToTestingActivity();
+            }
+        });
+    }
+
+    private void switchToTrainingActivity() {
+        Intent switchActivityIntent = new Intent(this, TrainingActivity.class);
+        startActivity(switchActivityIntent);
+    }
+    private void switchToTestingActivity() {
+        Intent switchActivityIntent = new Intent(this, TestingActivity.class);
+        startActivity(switchActivityIntent);
     }
 }
