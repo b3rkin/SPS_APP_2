5af2a51f4ab70d9046d91ea43c278ece53b2a47a
Pietervb2 <91535493+Pietervb2@users.noreply.github.com>
2022-05-23 11:45:14 +0200

    Update processor.py
    minor changes

--- Processing/processor.py
+++ Processing/processor.py
@@ -1,27 +1,42 @@
+from cgi import test
 from Process_func import *
+import glob
 
-cell_number = 5 # Number of cellss that are used in the model
+parentDirectory = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 
-# Format the training data by removing [] and adding missing commas.
-for i in range(cell_number):
-    dir = "TrainingData/trainMonday16/saved_data_cell" + str(i+1)+ "_Monday_East.txt"
-    cleanup_csv(dir)
-    format_txt(dir)
+cell_number = 15 # Number of cellss that are used in the model
+
+format = False
+pmfcreate = True
+addMissing = True
+
+pmf_dir = os.path.join(parentDirectory,"APP2_DATA_PROCESSING - COPY","MACpmf")
+
+if format:
+    # Format the training data by removing [] and adding missing commas.
+    for i in range(1,cell_number+1):
+        dir = os.path.join(parentDirectory, "GatherData_Bayes", "Friday2_dir", "saved_data_cell" + str(i) + "_Friday_All.txt")
+        cleanup_csv(dir)
+        format_txt(dir)
 
 # Empty the directory where the pmfs will be stored
-if (len(os.listdir("model"))!=0):
-    files = glob.glob('model/*')
+if (len(os.listdir(pmf_dir))!=0):
+    files = glob.glob(pmf_dir + '/*')
     for f in files:
         os.remove(f)
 
-# Create the pmfs for each MAC address and insert into the model/ directory
-for i in range(cell_number):
-    dir = "TrainingData/trainMonday16/saved_data_cell" + str(i+1) + "_Monday_East.txt"
-    create_pmf(dir,i)
 
-# Clean the model files by deleting [] and "" and add missing cells to the radar maps
-files = glob.glob('model/*')
-for f in files:
-    add_missing_cells(f, cell_number) # This function must come before cleanup_csv
-    cleanup_csv(f)  
+if pmfcreate:
+    # Create the pmfs for each MAC address and insert into the model/ directory
 
+    for i in range(1,cell_number+1):
+        dir = os.path.join(parentDirectory, "GatherData_Bayes", "All_dir", "saved_data_cell" + str(i) + "_totall.txt")
+        create_pmf(dir,i)
+    
+
+if addMissing:
+    # Clean the model files by deleting [] and "" and add missing cells to the radar maps
+    files = glob.glob(pmf_dir + '/*')
+    for f in files:
+        add_missing_cells(f, cell_number) # This function must come before cleanup_csv
+        cleanup_csv(f)  
