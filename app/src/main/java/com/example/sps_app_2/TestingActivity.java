fe8172de470dfbf349050de817de452928ef65d0
Pietervb2 <p.c.m.vanbentum@student.tudelft.nl>
2022-05-23 13:45:56 +0200

    start gui testing

--- app/src/main/java/com/example/sps_app_2/TestingActivity.java
+++ app/src/main/java/com/example/sps_app_2/TestingActivity.java
@@ -2,13 +2,40 @@ package com.example.sps_app_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TestingActivity extends AppCompatActivity {
+
+    private Button testingButton;
+    private TextView predictedCell;
+    private WifiManager wifiManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testing);
+
+        // Register the button and text box
+        testingButton = (Button) findViewById(R.id.button);
+        predictedCell = (TextView) findViewById(R.id.textView5);
+
+        // Button click to collect testing data
+        testingButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                // Wifi manager
+                wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+            3
+
+            }
+        });
+
+
+
     }
 }
