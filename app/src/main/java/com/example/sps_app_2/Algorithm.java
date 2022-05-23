a80ff2e21275a0387e26d9ebae21cdb8901e1ff1
b3rkin <zeybekogluberkin@metu.edu.tr>
2022-05-23 14:42:44 +0200

    Implementing the testing Activity

---
+++ app/src/main/java/com/example/sps_app_2/Algorithm.java
@@ -0,0 +1,19 @@
+package com.example.sps_app_2;
+
+import java.io. * ;
+import java.util.Scanner;
+import java.util.Scanner;
+import com.opencsv.CSVReader;
+import java.io.IOException;
+import java.io.FileReader;
+
+public class Algorithm {
+
+
+    void getLocation(String TestMacAddress, Integer maxSignalStrength) {
+
+    }
+
+
+
+}
--- app/src/main/java/com/example/sps_app_2/TestingActivity.java
+++ app/src/main/java/com/example/sps_app_2/TestingActivity.java
@@ -3,12 +3,23 @@ package com.example.sps_app_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import static com.example.sps_app_2.Algorithm.*;
+
+
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.crypto.Mac;
 
 public class TestingActivity extends AppCompatActivity {
 
@@ -22,15 +33,39 @@ public class TestingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_testing);
 
         // Register the button and text box
-        testingButton = (Button) findViewById(R.id.button);
-        predictedCell = (TextView) findViewById(R.id.textView5);
+        testingButton = (Button) findViewById(R.id.buttonTestMeasure);
+        predictedCell = (TextView) findViewById(R.id.textViewTestResult);
 
         // Button click to collect testing data
         testingButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
+                predictedCell.setText("Localizing...");
+
                 // Wifi manager
                 wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-            3
+
+                // Start scan
+                wifiManager.startScan();
+
+                List<ScanResult> scanResults = wifiManager.getScanResults();
+
+                Integer maxSignalStrength = -100;
+                String MacAddress = "";
+
+                for (ScanResult scanResult : scanResults) {
+
+                    Integer RSSI = scanResult.level;
+
+                    if(RSSI < maxSignalStrength ){
+                        maxSignalStrength = RSSI;
+                        MacAddress = scanResult.BSSID;
+                    }
+
+                }
+                // TODO: Need to implement getLocation algorithm that returns string with the result
+                String location = getLocation(MacAddress, maxSignalStrength);
+
+                predictedCell.setText(location);
 
             }
         });
