5ffea1cf66899fddc1a3302089f95b01733c16dd
Pietervb2 <91535493+Pietervb2@users.noreply.github.com>
2022-05-24 13:35:14 +0200

    Update Processing_func.py
    updata Proc_func

--- Processing/Processing_func.py
+++ Processing/Processing_func.py
@@ -110,17 +110,16 @@ def create_pmf(dir,cell, convolution = True, CONV_SCALING_1 = 0.5, CONV_SCALING_
 
     df_init = pd.read_csv(dir,header = None) # Read the cell data into a df
     sampleSize = df_init.shape[1]-1 # Maximum number of samples for a single measurement
-    print("cell number = ",cell)
     # Iterate over each row of the dataframe
     for index, row in df_init.iterrows():
         file_name_MAC = "MACpmf/" + row[0].replace(":","_") + ".csv" # File where the radar map will be placed, without the replace function couldn't save it on windows 
-        print(f' MAC address {row[0]} and cell {cell}')
-        if(exists(file_name_MAC)): # If the file already exists insert new row
 
+        if(exists(file_name_MAC)): # If the file already exists insert new row
             for j in range(sampleSize): # Obtain the histogram by increasing the array by index of the present signal
                 
                 if not pd.isna(row[j+1]): # Check if there is a sample at the index. not NaN
                     PMFBuffer[int(-row[j+1])] +=1
+            
                     if(convolution):
                         if(int(-row[j+1])+1)<100: 
                             PMFBufferTMP[int(-row[j+1])+1] += (1 * CONV_SCALING_1) 
@@ -144,29 +143,30 @@ def create_pmf(dir,cell, convolution = True, CONV_SCALING_1 = 0.5, CONV_SCALING_
                 sum = sum + PMFBuffer[j];  
 
             for j in range(len(PMFBuffer)): # Normalize histogram for pmf
-                PMFBuffer[j] = round(PMFBuffer[j]/float(sum),3)
-
-            if cell > 9:
-                df_tmp1 = {'Cells': "cellA"+str(cell), 'PMF':PMFBuffer} # Temp df
-                df_tmp = pd.DataFrame([['cellA' + str(cell), PMFBuffer]])    
-            else:
-                df_tmp1 = {'Cells': "cell"+str(cell), 'PMF':PMFBuffer} # Temp df    
-                df_tmp = pd.DataFrame([['cell' + str(cell), PMFBuffer]])   
+                PMFBuffer[j] = (PMFBuffer[j]/float(sum))
 
             df_tmp = pd.read_csv(file_name_MAC) # Read the map to insert new pmf
-            df_tmp = df_tmp.append(df_tmp1, ignore_index = True)
-            df_tmp.head().to_csv(file_name_MAC, index=False)
+    
+            if cell > 9:
+                df_tmp.loc[len(df_tmp)] = ["cellA" + str(cell), PMFBuffer]
+                 
+            else:
+                df_tmp.loc[len(df_tmp)] = ["cell" + str(cell), PMFBuffer]
+                
+            df_tmp.to_csv(file_name_MAC, index=False)
             PMFBuffer = [0 for q in range(100)]
+
             if convolution:
                 PMFBufferTMP = [0 for q in range(100)]
-
         else: # If the file does not exist, create and format the file
 
             for j in range(sampleSize): # Obtain the histogram by increasing the array by index of the present signal
                 
                 if not pd.isna(row[j+1]): # Check if there is a sample at the index. not NaN
+                   
                     PMFBuffer[int(-row[j+1])] +=1
-                        if(convolution):
+
+                    if(convolution):
                         if(int(-row[j+1])+1)<100: 
                             PMFBufferTMP[int(-row[j+1])+1] += (1 * CONV_SCALING_1) 
                         if(int(-row[j+1])-1>-1):
@@ -180,22 +180,21 @@ def create_pmf(dir,cell, convolution = True, CONV_SCALING_1 = 0.5, CONV_SCALING_
                         if(int(-row[j+1])-3>-1):
                             PMFBufferTMP[int(-row[j+1])-3] += (1 * CONV_SCALING_3)
             if convolution:
-                PMFBuffer += PMFBufferTMP 
+                PMFBuffer += PMFBufferTMP
+            
             # Obtain sum of the histogram for normalization
             sum = 0;     
             for j in range(0, len(PMFBuffer)):    
                 sum = sum + PMFBuffer[j]; 
 
-            print("normal sum", sum)
+            # print("normal sum", sum)
 
             for j in range(len(PMFBuffer)): # Normalize histogram for pmf
-                PMFBuffer[j] = round(PMFBuffer[j]/float(sum),3)
+                PMFBuffer[j] = (PMFBuffer[j]/float(sum))
             
             if cell > 9:
-                df_tmp1 = {'Cells': "cellA"+str(cell), 'PMF':PMFBuffer} # Temp df
                 df_tmp = pd.DataFrame([['cellA' + str(cell), PMFBuffer]])    
             else:
-                df_tmp1 = {'Cells': "cell"+str(cell), 'PMF':PMFBuffer} # Temp df    
                 df_tmp = pd.DataFrame([['cell' + str(cell), PMFBuffer]])
             
             df_tmp.columns = ['Cells', 'PMF']
@@ -278,13 +277,12 @@ def add_missing_cells(dir,cell_number):
     emptyPMF = [0. for i in range(100)] # Init empty pmf to insert for missing cells
     if(exists(dir)): 
         df = pd.read_csv(dir) # Read the cell data into a df
-        cells = df['Cells'].tolist() # Get list of present celss
+        cells = df['Cells'].tolist() # Get list of present cells
         for cell in range(1, cell_number+1):
 
             if cell > 9:
                 if not 'cellA'+ str(cell) in cells:
-                    df_tmp = {'Cells': 'cellA'+ str(cell), 'PMF':emptyPMF} # Temp df    
-                    df = df.append(df_tmp, ignore_index = True)
+                    df.loc[len(df.index)] = ["cellA" + str(cell),emptyPMF]
                     df = df.sort_values('Cells') # Sort so that rows go from cell1 to cell<cell_number> 
                     df.to_csv(dir, index=False, header = None)
                 else:
@@ -292,8 +290,7 @@ def add_missing_cells(dir,cell_number):
                 
             else:            
                 if not 'cell'+ str(cell) in cells:
-                    df_tmp = {'Cells': 'cell'+ str(cell), 'PMF':emptyPMF} # Temp df    
-                    df = df.append(df_tmp, ignore_index = True)
+                    df.loc[len(df.index)] = ["cell" + str(cell),emptyPMF]
                     df = df.sort_values('Cells') # Sort so that rows go from cell1 to cell<cell_number> 
                     df.to_csv(dir, index=False, header = None)
                 else:
@@ -303,14 +300,57 @@ def add_missing_cells(dir,cell_number):
         print("Directory does not exist")
 
 
+def check_pmf(dataCell, cell):
+
+    # Open the data collected for the cell
+    data = pd.read_csv(dataCell,header = None)
+
+    # Set mac as index 
+    macIndex = data.set_index([0])
+
+    # Get a list of all occuring mac addresses in that cell
+    listOfMac = list(macIndex.index.values)
+    
+    # Loop over these mac addresses 
+    for mac in listOfMac:
+        
+        rssiValuesRow = macIndex.loc[[mac]] # get all the occured rssi values in that cell for that mac
+        
+        csvToCheck = os.path.join("MACpmf",str(mac).replace(":","_") + ".csv")
+
+        # Select the csv file and check
+        macFile = pd.read_csv(csvToCheck,header = None)
+        macFileIndex = macFile.set_index([0])           # set the cells as the index
+        macCheckRow = macFileIndex.loc[[cell]]          # select the row of the corresponding cell 
+        
+        rssiValueOld = 0
+
+        for i in range(0,rssiValuesRow.shape[1]):
+            
+            rssiValue = rssiValuesRow.iloc[0,i] #obtained rssi value
+            if not pd.isna(rssiValue):
+                pmfIndexValue = macCheckRow.iloc[0,-int(rssiValue)]
+
+                if not pmfIndexValue > 0 and rssiValue != rssiValueOld:
+                    print(f'missing cell = {cell}, mac = {mac}, rssivalue = {rssiValue}, pmfindexvalue = {pmfIndexValue}')
+            rssiValueOld = rssiValue
+                
+            
+
+
+
 
 if __name__ == "__main__":
-    filter_top_mac("saved_data_cell1_Tuesday.csv",10, "toptest.txt")
-    
-    # testSaveData = pd.read_csv("saved_data_cell1_Tuesday.csv", header = None)
-    # testDuplicate = testSaveData[testSaveData.duplicated([0])]
-    
-    # if not testDuplicate.empty:
-    #     print("duplicate")
+    # filter_top_mac("saved_data_cell1_Tuesday.csv",10, "toptest.txt")
+    parentDirectory = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+    # pathFile = os.path.join(parentDirectory,"GatherData_Bayes","All_dir","saved_data_cell15_totall.txt")
 
+    # check_pmf(pathFile,"cellA15")
 
+    for i in range(1,16):
+        pathFile = os.path.join(parentDirectory,"GatherData_Bayes","All_dir","saved_data_cell" + str(i) +"_totall.txt")
+        
+        cell = "cell"
+        if i>9:
+            cell = "cellA"
+        check_pmf(pathFile,cell + str(i))
